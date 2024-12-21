84661956c69e318f3bd02c5bcd32e51ec01cdc46
What42Pizza <burg.what42@gmail.com>
2024-12-20 21:23:04 -0600

    wait no maybe this will work

--- src/lib.rs
+++ src/lib.rs
@@ -3,21 +3,24 @@
 
 
 
-// suggested prelude usage: `use hotrun_engine_api::{prelude as api, log, errors::{Result::*, StdResultFns}, ffi_String::{StrToFFI, StringToFFI}};`
+// suggested prelude usage:
+// `pub use hotrun_engine_api::{prelude as api, log, errors::{Result::*, StdResultFns}};`
+// `pub use hotrun_engine_api::ffi_String::{StrToFFI, StringToFFI};`
 
 #[cfg(not(feature = "is-engine-dep"))]
 pub mod prelude {
 	pub use crate::for_games::*;
 	pub use crate::shared::*;
 	pub use crate::errors::*;
-	pub use ffi_string::{self, *};
+	pub use ffi_string::*;
 }
 #[cfg(feature = "is-engine-dep")]
 pub mod prelude {
 	pub use crate::shared::*;
 	pub use crate::errors::*;
-	pub use ffi_string::{self, *};
+	pub use ffi_string::*;
 }
+pub use ffi_string::*;
 
 #[cfg(not(feature = "is-engine-dep"))]
 pub mod for_games;
